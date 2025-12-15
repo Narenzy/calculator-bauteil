1e89fcff8f83b7b3bd62d113000f9601b96eaaed
Taras Makhanko <mahankotaras@gmail.com>
2025-12-15 10:49:48 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,20 @@
+const langeEl = document.getElementById("lange-el");
+const dickeEl = document.getElementById("dicke-el");
+const breiteEl = document.getElementById("breite-el");
+const mengeEl = document.getElementById("menge-el");
+const outEl = document.getElementById("out-el");
+const out2El = document.getElementById("out2-el");
+const number = 500
+
+
+function calculate() {
+    const lange = parseFloat(langeEl.value)||0;
+    const dicke = parseFloat(dickeEl.value)||0;
+    const breite = parseFloat(breiteEl.value)||0;
+    const menge = parseFloat(mengeEl.value)||0;
+
+        const sum =  (menge * dicke) + (breite * 2) 
+        outEl.textContent = "Folie ist " + sum + " mm Breit"
+        const sum1 = lange + number
+        out2El.textContent = "Folie ist " + sum1 + "mm länge" 
+}
